02ace6a00b3414f9b9df1cd4abf9cb5fca159709
Antoni Dawiowicz <s27366@pjwstk.edu.pl>
2024-06-22 11:31:25 +0200

    yt

---
+++ DTOcw/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace DTOcw;
+
+public class SuperHero
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Place { get; set; }
+    public DateTime DateAdded { get; set; }
+    public DateTime? DateModyfied { get; set; }
+}
---
+++ DTOcw/SuperHeroController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DTOcw;
+
+[Route("api/controller")]
+[ApiController]
+public class SuperHeroController : ControllerBase
+{
+
+    private static List<SuperHero> heroes = new List<SuperHero>
+    {
+        new SuperHero
+        {
+            Id = 1,
+            Name = "asd",
+            FirstName = "asd",
+            LastName = "asd",
+            Place = "asd",
+            DateAdded = new DateTime(2001, 08, 10)
+        },
+        new SuperHero
+        {
+            Id = 2,
+            Name = "xyz",
+            FirstName = "xyz",
+            LastName = "xyz",
+            Place = "xyz",
+            DateAdded = new DateTime(2021, 08, 10)
+        }
+    };
+
+    [HttpGet]
+    public ActionResult<List<SuperHero>> GetHeroes()
+    {
+        return heroes;
+    }
+}
